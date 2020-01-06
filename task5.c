9ef7db88513c6f9c2aacfdd7144372938b1ae9b1
Elisa2105 <53063647+elisa2105@users.noreply.github.com>
2020-01-05 23:27:46 -0500

    nop function added

---
+++ task5.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * nopS -  doesn’t do anything.
+ * @stack: souble pointer
+ * @number_line: number of the line
+ * Return: void
+ */
+void nopS(stack_t **stack, unsigned int number_line)
+{
+	(void) stack;
+	(void) number_line;
+}
