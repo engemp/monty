176e7798eb6d70b7b9b18c826fffd20e485d9675
Elisa2105 <53063647+elisa2105@users.noreply.github.com>
2020-01-05 23:35:53 -0500

    sub function added

---
+++ task6.c
@@ -0,0 +1,17 @@
+/**
+ * subS - subtracts the first two items of the stack.
+ * @stack: double pointer
+ * @number_line: number of the line
+ * Return: void
+ */
+void subS(stack_t **stack, unsigned int number_line)
+{
+int cont = 0;
+(void) stack;
+(void) number_line;
+if (!caw->head || !caw->head->next)
+errorC(10);
+cont = caw->head->next->n - caw->head->n;
+caw->head->next->n = cont;
+popS(NULL, 0);
+}
