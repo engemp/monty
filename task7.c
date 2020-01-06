1d97a523d1863b47482180d063c642d12672edf7
Elisa2105 <53063647+elisa2105@users.noreply.github.com>
2020-01-05 23:51:45 -0500

    div funtion added

---
+++ task7.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+ * divS - divides the two items at the top of the stack
+ * @stack: double pointer
+ * @number_line: number of the line
+ * Return: void
+ */
+void divS(stack_t **stack, unsigned int number_line)
+{
+int res = 0;
+(void) stack;
+(void) line_number;
+if (!caw->head || !caw->head->next)
+error_check(11);
+if (caw->head->n == 0)
+error_check(12);
+res = caw->head->next->n / caw->head->n;
+caw->head->next->n = res;
+popS(NULL, 0);
+}
