3d31c48459c802839773cf47ac6ee33b32f89145
Elisa2105 <53063647+elisa2105@users.noreply.github.com>
2020-01-05 23:13:39 -0500

    adds the top two elements of the stack

---
+++ task4.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * addS - adds the top two elements of the stack
+ * @stack: double pointer
+ * @number_line: number of the line
+ * Return: void
+ */
+void addS(stack_t **stack, unsigned int number_line)
+{
+int cont = 0;
+(void) stack;
+(void) line_number;
+if (!caw->head || !caw->head->next)
+error_check(9);
+cont = caw->head->n + caw->head->next->n;
+caw->head->next->n = cont;
+popS(NULL, 0);
+}
